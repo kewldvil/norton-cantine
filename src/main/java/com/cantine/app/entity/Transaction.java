923c259b5b4fce35b9e2e6eeaea396e7336f5ba6
Sopheak <sopheak@sopheaks-MacBook-Pro.local>
2016-05-09 16:40:36 +0700

    create entities

--- src/main/java/com/cantine/app/entity/Transaction.java
+++ src/main/java/com/cantine/app/entity/Transaction.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -28,7 +29,12 @@ public class Transaction {
 	private float transactionAmountInDollar;
 	
 	@ManyToOne
+	@JoinColumn(name="userId")
 	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name="transactionTypeId")
+	private TransactionType transactionType;
 	
 
 }
--- src/main/java/com/cantine/app/entity/TransactionType.java
+++ src/main/java/com/cantine/app/entity/TransactionType.java
@@ -1,15 +1,11 @@
 package com.cantine.app.entity;
 
 import java.util.List;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -17,23 +13,15 @@ import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.SelectBeforeUpdate;
 
 @Entity
-@Table(name="TBL_USER")
+@Table(name="TBL_TRANSACTIONTYPE")
 @DynamicUpdate(value=true)
 @SelectBeforeUpdate(value=true) 
-public class User {
-	
+public class TransactionType {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int userId;
-	private String name;
-	private String username;
-	private String password;
-	private String userRole;
-	private boolean isActive=true;
-
-
-    @OneToMany(cascade=CascadeType.ALL,mappedBy="user")
+	private int transactionTypeId;
+	private String transactionTypeName;
+	
+	@OneToMany(mappedBy="transactionType")
 	private List<Transaction> transaction;
-	
-	
 }
